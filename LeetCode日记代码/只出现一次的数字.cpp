5c703a31fed55444c375ed7307854a865738bad7
幕冬儿 <56401150+EIheng@users.noreply.github.com>
2020-05-18 19:21:04 +0800

    Update 只出现一次的数字.cpp

--- LeetCode日记代码/只出现一次的数字.cpp
+++ LeetCode日记代码/只出现一次的数字.cpp
@@ -1,4 +1,19 @@
-﻿class Solution
+﻿
+/*
+什么是异或？
+　　0^0 = 0
+　　1^0 = 1
+　　0^1 = 1
+1^1 = 0
+两者不同则为1，两者相同为0。
+在计算机中：
+x: 0000 0001
+0: 0000 0000
+x^0=0000 0001
+也就是等于自身。
+x^x=0000 0000=0
+*/
+class Solution
 {
 public:
     int singleNumber(vector<int>& nums)
